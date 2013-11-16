f9d441517087a471017991411e3f662fdab68b20
Sebastian Seed <sebas.home1@gmail.com>
2013-11-16 13:25:15 +1100

    Added Amazon s3 storage login credentials to heroku enviroment vars - settings references them for security (not local hard code)

--- scoutsHerokuProject/settings.py
+++ scoutsHerokuProject/settings.py
@@ -181,8 +181,9 @@ if not DEBUG:
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL
-
-
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    
 # Static asset configuration
 #STATIC_ROOT = 'staticfiles'
 
