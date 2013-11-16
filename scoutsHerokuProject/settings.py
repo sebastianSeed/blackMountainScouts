527585cb4ed76fd3592d0a3765565886c564e3d4
Sebastian Seed <sebas.home1@gmail.com>
2013-11-16 14:30:27 +1100

    splitting static and media 2

--- scoutsHerokuProject/settings.py
+++ scoutsHerokuProject/settings.py
@@ -179,8 +179,7 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     DEFAULT_FILE_STORAGE = 'scoutsHerokuProject.s3utils.MediaRootS3BotoStorage'
     STATICFILES_STORAGE = 'scoutsHerokuProject.StaticRootS3BotoStorage'
-    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = S3_URL
+#    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     AWS_ACCESS_KEY_ID = os.environ['AWS_SECRET_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
