19319410b8c02253626e40348fee72e18b3bbed0
Sebastian Seed <sebas.home1@gmail.com>
2013-11-17 14:42:01 +1100

    Slideshow now pulls images from public images uploaded from admin

--- scoutsHerokuProject/urls.py
+++ scoutsHerokuProject/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from scoutsHerokuProject.SettingUtils import custom_discover
+from django.conf import settings
 
 
 #List of apps to add to admin
@@ -30,4 +29,7 @@ urlpatterns = patterns('',
    url(r'^admin/', include(admin.site.urls)),
 )
 
-
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+    )
