6de81c0d3c1c7e79287471c7a9cc922c8714ae45
Sebastian Seed <sebas.home1@gmail.com>
2013-11-23 17:56:48 +1100

    test fix for timeout

--- newsletter/models.py
+++ newsletter/models.py
@@ -45,7 +45,8 @@ class Newsletter(models.Model):
         for destination in email_destination:
             msg = EmailMultiAlternatives(subject, html_content , from_email, [destination,])
             msg.attach_alternative(text_content,"text/html")
-            msg.attach(self.title, open(self.newsletter, 'rb').read(), "application/pdf")
+            pdfAttachment = open(self.newsletter, 'rb').read()
+            msg.attach(self.title, pdfAttachment , "application/pdf")
             msg.send()
         super(Newsletter, self).save(*args, **kwargs)
        
