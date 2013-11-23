0b0445e6c6c57d247fa7987601928a7c85a33e00
Sebastian Seed <sebas.home1@gmail.com>
2013-11-23 18:23:17 +1100

    test

--- newsletter/models.py
+++ newsletter/models.py
@@ -45,8 +45,7 @@ class Newsletter(models.Model):
         for destination in email_destination:
             msg = EmailMultiAlternatives(subject, html_content , from_email, [destination,])
             msg.attach_alternative(text_content,"text/html")
-            msg.attach_file(self.newsletter.url)
-            print "URL IS  "+self.newsletter.url
+            msg.attach(self.title, self.newsletter.read() , "application/pdf")
             msg.send()
         super(Newsletter, self).save(*args, **kwargs)
        
