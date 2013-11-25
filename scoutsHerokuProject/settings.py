259778b6901dd2bcbc029c8f4a118307ea94dd90
Sebastian Seed <sebas.home1@gmail.com>
2013-11-25 19:57:06 +1100

    first attempt to debug media directory AMAZON S3 Bug

--- scoutsHerokuProject/settings.py
+++ scoutsHerokuProject/settings.py
@@ -20,7 +20,8 @@ if 'ONHEROKU' in os.environ:
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = "static"
     MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
-    MEDIA_URL = '//s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
+   # MEDIA_URL = '//s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = '//blackmountainstorage.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_ROOT = "/%s/" % STATIC_S3_PATH
     STATIC_URL = '//s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
